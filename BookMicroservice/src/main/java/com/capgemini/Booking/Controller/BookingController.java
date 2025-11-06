e4b1ee8804173bccda5faf30ae7b2bc67a02628f
LokeshNallam <68630405+LokeshNallam@users.noreply.github.com>
2025-11-06 12:43:23 +0530

    Add files via upload

---
+++ BookMicroservice/src/main/java/com/capgemini/Booking/Controller/BookingController.java
@@ -0,0 +1,121 @@
+package com.capgemini.Booking.Controller;
+
+import java.util.List;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.capgemini.Booking.Entity.Book;
+import com.capgemini.Booking.Entity.Inventory;
+import com.capgemini.Booking.Entity.Passenger;
+import com.capgemini.Booking.Exception.BookingException;
+import com.capgemini.Booking.Service.BookingServiceImpl;
+import com.capgemini.Booking.VO.BookAndFlight;
+
+@RestController
+@RequestMapping("api/book")
+public class BookingController 
+{
+	
+	
+	@Autowired
+	BookingServiceImpl bookingServiceImpl;
+	
+	@GetMapping("/get/{id}")
+	ResponseEntity<?> getbookingbyid(@PathVariable long id)
+	{
+		if(bookingServiceImpl.getBookingID(id).equals(null))
+		{
+			return new ResponseEntity<>("Enter the Correct Id",HttpStatus.BAD_REQUEST);
+		}
+
+		return new ResponseEntity<>(bookingServiceImpl.getBookingID(id),HttpStatus.OK);
+
+		
+	}
+	
+	@RequestMapping(value = "/getall",method = RequestMethod.GET)
+	public ResponseEntity<?> getAllBookingDetails()
+	{
+		return new ResponseEntity<> (bookingServiceImpl.getAllBookingDetails(),HttpStatus.OK);
+		
+	}
+	
+	@PostMapping("/post")
+	long updateInventory(@RequestBody Book book)
+	{
+		System.out.println("Booking Requesting" + book);
+		return bookingServiceImpl.updateInventory(book);
+	}
+	
+	@GetMapping("/getinventory")
+	List<Inventory> getinventory(){
+		return bookingServiceImpl.getInventory();
+	}
+	@GetMapping("/getpassenger")
+	List<Passenger> getpassenger(){
+		return bookingServiceImpl.getPassenger();
+	}
+	
+	@GetMapping("/flight/{id}")
+	public ResponseEntity<?> getFlightwithbook(@PathVariable("id") long BookId) {
+		return  new ResponseEntity<>(bookingServiceImpl.getFlightwithbook(BookId),HttpStatus.OK);
+	}
+	
+	@PutMapping("/puttbook")
+    public Book updateBooking(@RequestBody Book  book)
+    {
+        return bookingServiceImpl.updateBooking(book);
+    }
+	
+	@DeleteMapping("/deletebyid/{id}")
+    public ResponseEntity<?> Deletebyid(@PathVariable ("id") long id)
+    {
+		try {
+        bookingServiceImpl.Deletebyid(id);
+        return new ResponseEntity<>("Deleted:"+id,HttpStatus.OK);
+		}
+		catch(Exception e)
+		{
+			return new ResponseEntity<>("Please enter the correct the Booking Id",HttpStatus.BAD_REQUEST);
+		}
+    }
+	
+	@PostMapping("/addbooking")
+    public ResponseEntity<Book> add(@RequestBody Book add) throws NoSuchElementException
+    {
+        Book upd= bookingServiceImpl.updateBooking(add);
+        return new ResponseEntity<Book>(upd,HttpStatus.CREATED);
+    }
+	
+	@RequestMapping(value="/create" , method = RequestMethod.POST)
+	public ResponseEntity<?> book(@RequestBody Book record) throws BookingException{
+		try {
+		System.out.println("Booking Request" + record); 
+		return new ResponseEntity<>( bookingServiceImpl.book(record),HttpStatus.OK);
+		}
+		catch(BookingException e)
+		{
+			return new ResponseEntity<>("please enter correct details", HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	
+	
+}
---
+++ BookMicroservice/src/main/java/com/capgemini/Booking/Service/BookingServiceImpl.java
@@ -0,0 +1,218 @@
+package com.capgemini.Booking.Service;
+
+import java.util.Date;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+import com.capgemini.Booking.Entity.Book;
+import com.capgemini.Booking.Entity.Inventory;
+import com.capgemini.Booking.Entity.Passenger;
+import com.capgemini.Booking.Exception.BookingException;
+import com.capgemini.Booking.Repository.BookingReposoitory;
+import com.capgemini.Booking.Repository.InventoryRepository;
+import com.capgemini.Booking.Repository.PassengerRepository;
+import com.capgemini.Booking.VO.Flight;
+
+import lombok.extern.slf4j.Slf4j;
+
+import com.capgemini.Booking.VO.BookAndFlight;
+import com.capgemini.Booking.VO.Fare;
+
+
+@Slf4j
+@Service
+public class BookingServiceImpl implements IBookingService
+{
+	
+	private static final String FareURL = "http://localhost:8080/api/fare/getfare";
+	
+	@Autowired
+	private BookingReposoitory bookingReposoitory;
+	
+	@Autowired
+	private InventoryRepository inventoryRepository;
+	
+	@Autowired
+	private PassengerRepository passengerRepository;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
+	
+	
+	@Override
+	public Book getBookingID(long id) {
+		
+		Book books = bookingReposoitory.findById(id).get();
+		books.setTotalFare(books.getFare()*books.getPassenger().size());
+		      
+		return bookingReposoitory.findById(new Long(id)).get();
+		
+		
+		}
+
+	@Override
+	public List<Book> getAllBookingDetails() {
+		
+		return bookingReposoitory.findAll();
+	}
+
+
+	@Override
+	public void updatestatus(String status, long bookingid) {
+		//Book book = bookingReposoitory.findById(new Long (bookingid)).get();
+		//book.setStatus(status);
+	}
+
+	@Override
+	public List<Inventory> getInventory() {
+		 return inventoryRepository.findAll();
+    
+	}
+
+	@Override
+	public List<Passenger> getPassenger() {
+		
+
+		return passengerRepository.findAll();
+	}
+
+	@Override
+	public Book updateBooking(Book book) 
+	{
+	    Optional<Book> books = bookingReposoitory.findById(book.getBookId());
+        if(books.isPresent())
+         {
+             Book bookss=books.get();
+             bookss.setBookId(book.getBookId());
+             bookss.setBookingDate(book.getBookingDate());
+             bookss.setDestination(book.getDestination());
+             bookss.setOrigin(book.getOrigin());
+             bookss.setFare(book.getFare());
+             bookss.setFlightDate(book.getFlightDate());
+             bookss.setFlightNumber(book.getFlightNumber());
+             bookss.setStatus(book.getStatus());
+
+             Book added = bookingReposoitory.save(bookss);
+             return added;
+         }
+        else {
+            Book booking= bookingReposoitory.save(book);
+            return booking;
+
+        }
+   
+	}
+
+	public long updateInventory(Book book) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public BookAndFlight getFlightwithbook(long flightNumber) {
+		BookAndFlight vo = new BookAndFlight();
+		Book book = bookingReposoitory.findByflightNumber(flightNumber);
+		book.setTotalFare(book.getFare()*book.getPassenger().size());
+		Flight flight = restTemplate.getForObject("http://localhost:8090/api/flight/flightNumber/" +book.getFlightNumber(), Flight.class);
+		vo.setBook(book);
+		vo.setFlight(flight);
+		return vo;
+	}
+	
+	@Override
+	public ResponseEntity<?> book(Book record) throws BookingException {
+	
+		
+		Fare fare = restTemplate.getForObject("http://localhost:8080/api/fare/getfare/" +record.getFlightNumber() +"/" +record.getFlightDate(),Fare.class);		
+		/*  if (record.getFare()==fare.getFlightFare()) throw new
+		  BookingException("fare is tampered");*/
+		  
+		  if(record.getPassenger().isEmpty()) {
+			  throw new BookingException("Passenger are empty");
+		  }
+		  
+		  record.setFare(fare.getFlightFare());
+		 
+		
+		Inventory inventory = inventoryRepository.findByFlightNumberAndFlightDate(record.getFlightNumber(),record.getFlightDate());
+		if(!inventory.isAvailable(record.getPassenger().size())){
+			throw new BookingException("No more seats avaialble");
+		}
+	
+		inventory.setAvailable(inventory.getAvailable() - record.getPassenger().size());
+		inventoryRepository.saveAndFlush(inventory);
+	
+		record.setStatus(BookingStatus.BOOKING_CONFIRMED); 
+		List<Passenger> passengers = record.getPassenger();
+		passengers.forEach(passenger -> passenger.setBook(record));
+		record.setBookingDate(new Date());
+		long id=  bookingReposoitory.save(record).getBookId();
+		long passengersize= record.getPassenger().size();
+		long Pfare=record.getFare()*passengersize;
+		//record.setFare(Pfare);
+		record.setTotalFare(Pfare);
+		
+		return new ResponseEntity<>("bookingid=" +id + "and" + "" +"Fare="+Pfare ,HttpStatus.ACCEPTED);
+	}
+
+	
+
+	@Override
+	public Book findByFlightNumber(long flightNumber) {
+		// TODO Auto-generated method stub
+		return bookingReposoitory.findByflightNumber(flightNumber);
+	}
+
+	@Override
+	public String Deletebyid(long id) {
+		   Optional<Book> enti=bookingReposoitory.findById(id);
+	        if(enti.isPresent())
+	        {
+	        	Book newone= enti.get();
+	        	int size = newone.getPassenger().size();
+	        	long fnumber=newone.getFlightNumber();
+	        	Inventory inv=inventoryRepository.findByFlightNumber(fnumber);
+	        	int inventory=inv.getAvailable();
+	        	inv.setAvailable(inventory+size);
+	            bookingReposoitory.deleteById(id);
+	            return "successfully deleted";
+	        }
+	        else
+	        {
+	            throw new NoSuchElementException("enter the correct id");
+	        }
+	}
+
+	@Override
+	public Book addbooking(Book book) {
+		// TODO Auto-generated method stub
+		
+		return bookingReposoitory.save(book);
+	}
+
+
+
+
+	
+	
+	
+	
+
+}
